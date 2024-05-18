e71e7d30f950de302a473a8c8a39aaf94020f77b
Abhishek Parochi <abhishek.parochi.ex21@ggits.net>
2024-05-18 19:53:15 +0530

    Initial commit

---
+++ CPU1_RAM/lab_main.d
@@ -0,0 +1,270 @@
+# FIXED
+
+lab_main.obj: ../lab_main.c
+lab_main.obj: syscfg/board.h
+lab_main.obj: D:/Users/VICTUS/workspace_v12/adc_lp_f2837xd/device/driverlib.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_memmap.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/adc.h
+lab_main.obj: D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/stdbool.h
+lab_main.obj: D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/_ti_config.h
+lab_main.obj: D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/linkage.h
+lab_main.obj: D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/stdint.h
+lab_main.obj: D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/_stdint40.h
+lab_main.obj: D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/stdint.h
+lab_main.obj: D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/cdefs.h
+lab_main.obj: D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/_types.h
+lab_main.obj: D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/machine/_types.h
+lab_main.obj: D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/machine/_stdint.h
+lab_main.obj: D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/_stdint.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_adc.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_sysctl.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_types.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/cpu.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/debug.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/asysctl.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_asysctl.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/can.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_can.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/sysctl.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_nmi.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_otp.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/interrupt.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_ints.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_pie.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/cla.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_cla.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/clb.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_clb.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/cmpss.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_cmpss.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/cputimer.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_cputimer.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/dac.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_dac.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/dcsm.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_dcsm.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/dma.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_dma.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/ecap.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_ecap.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/emif.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_emif.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_memcfg.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/epwm.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_epwm.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/eqep.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_eqep.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/flash.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_flash.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/gpio.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_gpio.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_xint.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/xbar.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_clbxbar.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_epwmxbar.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_inputxbar.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_outputxbar.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_xbar.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/hrpwm.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_hrpwm.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/hrpwm.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/i2c.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_i2c.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/hw_reg_inclusive_terminology.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/ipc.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_ipc.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/mcbsp.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_mcbsp.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/memcfg.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/pin_map.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/pin_map_legacy.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/sci.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_sci.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/sdfm.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_sdfm.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/spi.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_spi.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/upp.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_upp.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/version.h
+lab_main.obj: D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/driver_inclusive_terminology_mapping.h
+lab_main.obj: D:/Users/VICTUS/workspace_v12/adc_lp_f2837xd/device/device.h
+
+../lab_main.c:
+
+syscfg/board.h:
+
+D:/Users/VICTUS/workspace_v12/adc_lp_f2837xd/device/driverlib.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_memmap.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/adc.h:
+
+D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/stdbool.h:
+
+D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/_ti_config.h:
+
+D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/linkage.h:
+
+D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/stdint.h:
+
+D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/_stdint40.h:
+
+D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/stdint.h:
+
+D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/cdefs.h:
+
+D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/_types.h:
+
+D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/machine/_types.h:
+
+D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/machine/_stdint.h:
+
+D:/ti/ccs1240/ccs/tools/compiler/ti-cgt-c2000_22.6.0.LTS/include/sys/_stdint.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_adc.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_sysctl.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_types.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/cpu.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/debug.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/asysctl.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_asysctl.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/can.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_can.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/sysctl.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_nmi.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_otp.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/interrupt.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_ints.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_pie.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/cla.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_cla.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/clb.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_clb.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/cmpss.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_cmpss.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/cputimer.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_cputimer.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/dac.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_dac.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/dcsm.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_dcsm.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/dma.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_dma.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/ecap.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_ecap.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/emif.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_emif.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_memcfg.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/epwm.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_epwm.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/eqep.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_eqep.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/flash.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_flash.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/gpio.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_gpio.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_xint.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/xbar.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_clbxbar.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_epwmxbar.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_inputxbar.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_outputxbar.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_xbar.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/hrpwm.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_hrpwm.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/hrpwm.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/i2c.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_i2c.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/hw_reg_inclusive_terminology.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/ipc.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_ipc.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/mcbsp.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_mcbsp.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/memcfg.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/pin_map.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/pin_map_legacy.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/sci.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_sci.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/sdfm.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_sdfm.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/spi.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_spi.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/upp.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/inc/hw_upp.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/version.h:
+
+D:/ti/c2000/C2000Ware_5_02_00_00/driverlib/f2837xd/driverlib/driver_inclusive_terminology_mapping.h:
+
+D:/Users/VICTUS/workspace_v12/adc_lp_f2837xd/device/device.h:
+
